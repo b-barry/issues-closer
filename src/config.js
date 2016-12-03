a08aa880d9d837b32c74401b47e6a26787d18138
Boubacar S. BARRY <bouba@hackages.io>
2016-12-03 15:49:11 +0100

    Add default export

--- src/config.js
+++ src/config.js
@@ -7,3 +7,4 @@ export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID.trim();
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET.trim();
 export const MONGO_URL = process.env.MONGO_URL.trim();
 
+export default { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, MONGO_URL};
