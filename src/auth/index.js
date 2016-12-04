7a6aeb2163d293089c550dbb449e827dee42dc4b
Boubacar S. BARRY <bouba@hackages.io>
2016-12-04 18:09:38 +0100

    Setup Hapi Auth Jwt 2

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,21 +1,52 @@
 import {getAccessToken} from "./auth";
 import UserModel from "./user.model";
+import {validateJwtToken} from './utils'
 
-export default ({server, dps : {Github, Config, Mongoose}}) => {
+export default ({server, dps : {Github, Config, Mongoose, HapiAuthJwt2}}) => {
 
   const User = UserModel({Mongoose});
 
-  server.route({
-    method: 'POST',
-    path: '/auth/accessToken',
-    // TODO: fix it after test
-    config: {
-      cors: {
-        origin: ['*'],
-        additionalHeaders: ['cache-control', 'x-requested-with']
-      }
+  server.route([
+    {
+      method: 'POST',
+      path: '/auth/accessToken',
+
+      config: {
+        auth: false,
+        // TODO: fix it after test
+        cors: {
+          origin: ['*'],
+          additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+      },
+      handler: getAccessToken({Github, Config, DB: {User}})
     },
-    handler: getAccessToken({Github, Config, DB:{ User}})
+    // For testing purpose
+    {
+      method: 'GET', path: '/restricted', config: {auth: 'jwt'},
+      handler: function (request, reply) {
+        reply({text: 'You used a Token!'})
+          .header("Authorization", request.headers.authorization);
+      }
+    }
+  ]);
+
+
+  server.register(HapiAuthJwt2, function (err) {
+
+    if (err) {
+      console.log(err);
+    }
+
+    server.auth.strategy('jwt', 'jwt',
+      {
+        key: Config.SECRET_TOKEN,
+        validateFunc: validateJwtToken({User}),
+        verifyOptions: {algorithms: ['HS256'], ignoreExpiration: false}
+      });
+
+    server.auth.default('jwt');
+
   });
 }
 
