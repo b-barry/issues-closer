a7bd0d694def78d88bad8d4270dfc6e326849483
Boubacar S. BARRY <bouba@hackages.io>
2016-12-04 11:13:47 +0100

    Add mongoose and implement connection to the database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {Server} from 'hapi';
+import Mongoose from 'mongoose';
 import {
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
@@ -6,8 +7,12 @@ import {
 } from './config';
 
 
+Mongoose.connect(MONGO_URL);
+const DB = Mongoose.connection;
 // Create a server with a host and port
 const server = new Server();
+
+
 server.connection({
   host: 'localhost',
   port: 8000
@@ -24,11 +29,15 @@ server.route({
   }
 });
 
-// Start the server
-server.start((err) => {
-
-  if (err) {
-    throw err;
-  }
-  console.log('Server running at:', server.info.uri);
+DB.on('error', console.error.bind(console, 'connection with database  error'));
+DB.once('open', () =>{
+  console.log("Connection with database succeeded.");
+  // Start the server
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+  });
 });
+
