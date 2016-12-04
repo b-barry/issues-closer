c37dab3927eee5e6a2aef2420f1abcc73d555ce8
Boubacar S. BARRY <bouba@hackages.io>
2016-12-04 13:50:00 +0100

    Add logging system

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import {Server} from 'hapi';
-import Mongoose from 'mongoose';
-import Config, {
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
-  MONGO_URL
-} from './config';
-import Promisify from 'promisify-node';
-import Github  from 'octonode';
-import  auth from './auth';
+import {Server} from "hapi";
+import Mongoose from "mongoose";
+import good from "good";
+import goodConsole from "good-console";
+import Config, {MONGO_URL} from "./config";
+import Promisify from "promisify-node";
+import Github from "octonode";
+import auth from "./auth";
 
 
 Promisify(Github);
-
 
 
 Mongoose.connect(MONGO_URL);
@@ -24,6 +21,7 @@ server.connection({
   host: 'localhost',
   port: 8000
 });
+
 
 // Add the route
 server.route({
@@ -46,9 +44,28 @@ auth({
   }
 });
 
-DB.on('error', console.error.bind(console, 'connection with database  error'));
-DB.once('open', () =>{
-  console.log("Connection with database succeeded.");
+
+/**
+ * Good Options
+ *
+ */
+const goodOptions = {
+
+  reporters: {
+    console: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{
+        log: '*',
+        response: '*'
+      }]
+    }, {
+      module: 'good-console'
+    }, 'stdout']
+  }
+};
+
+const startServer = (server) => {
   // Start the server
   server.start((err) => {
     if (err) {
@@ -56,5 +73,31 @@ DB.once('open', () =>{
     }
     console.log('Server running at:', server.info.uri);
   });
-});
+};
 
+
+/**
+ *  Server plugins
+ */
+
+const registers = [
+  {
+    register: good,
+    options: goodOptions,
+  }
+];
+
+
+server
+  .register(registers, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    DB.on('error', console.error.bind(console, 'connection with database  error'));
+    DB.once('open', () => {
+      console.log("Connection with database succeeded.");
+      startServer(server)
+    });
+
+  });
