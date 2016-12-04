b94f04e93d619500f500572b65503cd2259f8de1
Boubacar S. BARRY <bouba@hackages.io>
2016-12-04 17:12:02 +0100

    Implement getAccessToken

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,24 +1,105 @@
-export const getAccessToken = ({Github,  Config}) => {
+import {Observable} from "rxjs";
+import {getNextPageNumberFromLinkHeader} from "./utils";
 
-  const  auth_url = Github.auth.config({
-    id:Config.GITHUB_CLIENT_ID,
+export const getAccessToken = ({Github, Config, DB}) => {
+
+
+  const auth_url = Github.auth.config({
+    id: Config.GITHUB_CLIENT_ID,
     secret: Config.GITHUB_CLIENT_SECRET,
   });
 
-  return (req, res) => {
+  const getMeInfo = (token) => {
+    console.log('Token getMeInfo ', token);
+    const client = Github.client(token);
+    return Observable.from(client.me().infoAsync());
+  };
+
+  const getMeRepository = (token, page = 1, per_page = 100) => {
+    console.log('Token getMeRepository ', token)
+    const client = Github.client(token);
+
+    return Observable.from(client.me().reposAsync(page, per_page))
+      .mergeMap(([repos, header]) => {
+
+        const nextPage = getNextPageNumberFromLinkHeader(header.link);
+
+        const response$ = Observable.of(repos);
+        const next$ = nextPage ? getMeRepository(token, nextPage) : Observable.empty();
+
+        return Observable.merge(response$, next$)
+      })
+      .reduce((acc, next) => {
+        return acc.concat(next);
+      });
+  };
+
+  return (req, reply) => {
+
+    console.log(' Enter in getAccessToken ');
+
     const {payload:body} = req;
 
-    Github.auth
-      .login(body.code)
-      .then((token)=>{
-        return Github.client(token).me().info();
-      })
-      .then((user)=>{
-        return res(user);
-      })
-      .catch((err)=>{
-        console.log(' Error acces token ', {err:err});
-        return res('Getting access token error');
+    const accessToken$ = Observable.from(Github.auth.loginAsync(body.code)).share();
+
+    const getInfo$ = accessToken$
+      .switchMap((token) => {
+        return getMeInfo(token);
+      });
+
+    const getRepo$ = accessToken$
+      .switchMap((token) => {
+        return getMeRepository(token);
+      });
+    const createOrUpdateUser$ = ([info], token) => {
+      console.log('Infos ', info, ' Token : ', token);
+
+      const findByEmail$ = Observable.from(DB.User.findOne({email: info.email}).exec());
+
+      return findByEmail$
+        .mergeMap((user) => {
+          if (user) {
+            user.access_token_gh = token;
+            user.updated_at = Date.now();
+            return Observable.from(user.save())
+          }
+          const newUser = new DB.User({
+            email: info.email,
+            access_token_gh: token
+          });
+
+          return Observable.from(newUser.save());
+        })
+        .catch((err) => {
+          console.log('Error findByEmail$ stream', err);
+          return Observable.throw('Error findByEmail$ stream');
+        });
+    };
+
+    const getUser$ = getInfo$
+      .withLatestFrom(accessToken$, createOrUpdateUser$)
+      .mergeMap((user) => {
+        return user
+      });
+
+
+    const response$ = Observable
+      .combineLatest(getUser$, getRepo$, (user, repos) => {
+        return {
+          token: user.email,
+          repos: repos
+        }
+      });
+
+    response$
+      .subscribe((res) => {
+        //console.log(' Next getAccessToken ', res);
+        reply(res);
+      }, (err) => {
+        console.log('Err end ', err);
+        reply(new Error('Error getAccessToken'))
+      }, () => {
+        console.log('Complete getAccessToken')
       });
   };
 };
